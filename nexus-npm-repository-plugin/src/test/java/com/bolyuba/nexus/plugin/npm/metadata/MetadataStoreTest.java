sonatype/nexus-npm-repository-plugin

commit afc59ff0bcac3f56e54f272df3248e3077124ae2

NX-620: Concurrency issue with document updates
Using UID locking for now, as it seems NPM eagerly
requests even on multiple threads (as transitive deps
are resolved) for same package, that in some rare cases
causes concurrency updates that fail due to optimistic
locking in Orient.

nexus-npm-repository-plugin/src/main/java/com/bolyuba/nexus/plugin/npm/metadata/internal/orient/OrientMetadataStore.java
@@ -11,6 +11,8 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.sonatype.nexus.configuration.application.ApplicationDirectories;
+import org.sonatype.nexus.proxy.access.Action;
+import org.sonatype.nexus.proxy.item.RepositoryItemUidLock;
 import org.sonatype.sisu.goodies.lifecycle.LifecycleSupport;
 
 import com.bolyuba.nexus.plugin.npm.NpmRepository;
@@ -183,6 +185,8 @@ public class OrientMetadataStore
     checkNotNull(repository);
     checkNotNull(packageRoot);
     final EntityHandler<PackageRoot> entityHandler = getHandlerFor(PackageRoot.class);
+    final RepositoryItemUidLock lock = repository.createUid(packageRoot.getName()).getLock();
+    lock.lock(Action.update);
     try (ODatabaseDocumentTx db = db()) {
       db.begin();
       try {
@@ -191,6 +195,9 @@ public class OrientMetadataStore
       finally {
         db.commit();
       }
+    }
+    finally {
+      lock.unlock();
     }
   }
 

commit 664faf46e1da6eb832c43248b4995e6491ac25dc

NX-620: Fixing the build
Tests, but not prod code got broken by change introduced in
afc59ff0bcac3f56e54f272df3248e3077124ae2

nexus-npm-repository-plugin/src/test/java/com/bolyuba/nexus/plugin/npm/metadata/MetadataStoreTest.java
@@ -12,7 +12,10 @@ import org.sonatype.nexus.apachehttpclient.Hc4Provider;
 import org.sonatype.nexus.configuration.application.ApplicationDirectories;
 import org.sonatype.nexus.proxy.ResourceStoreRequest;
 import org.sonatype.nexus.proxy.item.ContentLocator;
+import org.sonatype.nexus.proxy.item.DefaultRepositoryItemUid;
 import org.sonatype.nexus.proxy.item.PreparedContentLocator;
+import org.sonatype.nexus.proxy.item.RepositoryItemUid;
+import org.sonatype.nexus.proxy.item.RepositoryItemUidLock;
 import org.sonatype.nexus.proxy.item.StringContentLocator;
 import org.sonatype.nexus.proxy.registry.ContentClass;
 import org.sonatype.nexus.proxy.repository.ProxyRepository;
@@ -127,6 +130,10 @@ public class MetadataStoreTest
 
     tarballSource = new TarballSourceImpl(applicationDirectories, httpTarballTransport, ImmutableMap.<String, TarballValidator>of("size", new SizePayloadValidator(), "sha1", new Sha1HashPayloadValidator()));
 
+    // dummy uid and uidLock
+    final RepositoryItemUid uid = mock(RepositoryItemUid.class);
+    when(uid.getLock()).thenReturn(mock(RepositoryItemUidLock.class));
+
     // not using mock as it would OOM when it tracks invocations, as we work with large files here
     npmHostedRepository = new DefaultNpmHostedRepository(mock(ContentClass.class), mock(
         NpmHostedRepositoryConfigurator.class), metadataService)
@@ -135,6 +142,9 @@ public class MetadataStoreTest
       public String getId() {
         return "hosted";
       }
+
+      @Override
+      public RepositoryItemUid createUid(String path) { return uid; }
     };
 
     // not using mock as it would OOM when it tracks invocations, as we work with large files here
@@ -154,6 +164,9 @@ public class MetadataStoreTest
 
       @Override
       public String getRemoteUrl() { return "http://registry.npmjs.org/"; }
+
+      @Override
+      public RepositoryItemUid createUid(String path) { return uid; }
     };
 
     // not using mock as it would OOM when it tracks invocations, as we work with large files here