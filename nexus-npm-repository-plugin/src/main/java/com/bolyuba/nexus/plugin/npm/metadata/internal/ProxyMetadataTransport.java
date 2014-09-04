16f002474c647e559727947e41c84a25fe5d96ab
Tamas Cservenak <tamas@cservenak.net>
2014-09-04 13:46:12 +0200

    Javadoc

--- nexus-npm-repository-plugin/src/main/java/com/bolyuba/nexus/plugin/npm/metadata/internal/ProxyMetadataTransport.java
+++ nexus-npm-repository-plugin/src/main/java/com/bolyuba/nexus/plugin/npm/metadata/internal/ProxyMetadataTransport.java
@@ -17,7 +17,10 @@ public interface ProxyMetadataTransport
   PackageRootIterator fetchRegistryRoot(final NpmProxyRepository npmProxyRepository) throws IOException;
 
   /**
-   * Fetches one single package root of the proxied {@link NpmProxyRepository}.
+   * Fetches one single package root of the proxied {@link NpmProxyRepository}. Supplied reoository and package name
+   * must not be {@code null}s, while the expired packageRoot might be {@code null}. If present, metadata from it
+   * like ETag will be used to make a "conditional GET", and if remote unchanged, the passed in expired instance
+   * is returned.
    */
   PackageRoot fetchPackageRoot(final NpmProxyRepository npmProxyRepository, final String packageName,
                                final PackageRoot expired) throws IOException;
